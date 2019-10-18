0f92ecabd813a201ca5967a9bc8022ca0faccd32
thingsym <thingsym@gmail.com>
2019-10-18 09:11:29 +0900

    fix: fix archive title

--- templates/archive/topics.php
+++ templates/archive/topics.php
@@ -14,11 +14,7 @@
 	<div class="article-inner">
 	<header class="entry-header">
 		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+		the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 
 		if ( 'post' === get_post_type() ) :
 			?>
