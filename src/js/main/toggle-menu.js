67b232d7def7234d740636bec0030d7ce46e880e
thingsym <thingsym@gmail.com>
2020-04-13 16:44:37 +0900

    perf: change from global scope to Immediate function

--- src/js/main/toggle-menu.js
+++ src/js/main/toggle-menu.js
@@ -1,53 +1,55 @@
-var ToggleMenu = function() {
-	var menu = document.querySelector('.container .site-navi');
-	var button = document.querySelector('#js-drawer-btn');
-	var overlay = document.querySelector('.drawer-overlay');
+( function() {
+  var ToggleMenu = function() {
+    var menu = document.querySelector('.container .site-navi');
+    var button = document.querySelector('#js-drawer-btn');
+    var overlay = document.querySelector('.drawer-overlay');
 
-	if ( ! menu && button ) {
-		button.parentNode.removeChild(button);
-		return;
-	}
+    if ( ! menu && button ) {
+      button.parentNode.removeChild(button);
+      return;
+    }
 
-	if ( ! button )
-		return;
+    if ( ! button )
+      return;
 
-	button.addEventListener( 'click', this.toggle_drawer, false );
-	overlay.addEventListener( 'click', this.toggle_drawer, false );
+    button.addEventListener( 'click', this.toggle_drawer, false );
+    overlay.addEventListener( 'click', this.toggle_drawer, false );
 
-	button.addEventListener( 'keydown', event => {
-		// Enter key
-		if ( event.keyCode === 13 ) {
-			document.body.classList.toggle('drawer--on');
-			document.querySelector('.site-content').setAttribute('tabindex', 0);
-		}
-	});
+    button.addEventListener( 'keydown', event => {
+      // Enter key
+      if ( event.keyCode === 13 ) {
+        document.body.classList.toggle('drawer--on');
+        document.querySelector('.site-content').setAttribute('tabindex', 0);
+      }
+    });
 
-	document.body.addEventListener('keydown', event => {
-		if (document.body.classList.contains('drawer--on')) {
-			// Tab key, out of focus of drawer
-			if ( event.keyCode === 9 && document.activeElement.classList.contains('site-content') ) {
-				document.body.classList.toggle('drawer--on');
-				document.querySelector('.site-content').removeAttribute('tabindex');
-			}
-			// Esc key
-			if ( event.keyCode === 27 ) {
-				document.body.classList.toggle('drawer--on');
-				document.querySelector('.site-content').removeAttribute('tabindex');
-				button.focus();
-			}
-		}
-	});
-}
+    document.body.addEventListener('keydown', event => {
+      if (document.body.classList.contains('drawer--on')) {
+        // Tab key, out of focus of drawer
+        if ( event.keyCode === 9 && document.activeElement.classList.contains('site-content') ) {
+          document.body.classList.toggle('drawer--on');
+          document.querySelector('.site-content').removeAttribute('tabindex');
+        }
+        // Esc key
+        if ( event.keyCode === 27 ) {
+          document.body.classList.toggle('drawer--on');
+          document.querySelector('.site-content').removeAttribute('tabindex');
+          button.focus();
+        }
+      }
+    });
+  }
 
-ToggleMenu.prototype.toggle_drawer = function() {
-	document.body.classList.toggle('drawer--on');
+  ToggleMenu.prototype.toggle_drawer = function() {
+    document.body.classList.toggle('drawer--on');
 
-	if (document.body.classList.contains('drawer--on')) {
-		document.querySelector('.site-content').setAttribute('tabindex', 0);
-	}
-	else {
-		document.querySelector('.site-content').removeAttribute('tabindex');
-	}
-}
+    if (document.body.classList.contains('drawer--on')) {
+      document.querySelector('.site-content').setAttribute('tabindex', 0);
+    }
+    else {
+      document.querySelector('.site-content').removeAttribute('tabindex');
+    }
+  }
 
-new ToggleMenu();
+  new ToggleMenu();
+} )();
