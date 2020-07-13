5ce634b33ce52eefbe04de3917c54bdfdba82a89
thingsym <thingsym@gmail.com>
2020-07-13 16:58:33 +0900

    fix: comment title heading level down one

--- templates/parts/comments.php
+++ templates/parts/comments.php
@@ -26,7 +26,7 @@ if ( post_password_required() ) {
 	// You can start editing here -- including this comment!
 	if ( have_comments() ) :
 		?>
-		<h2 class="comments-title">
+		<h3 class="comments-title">
 			<?php
 			$comment_count = get_comments_number();
 			if ( '1' === $comment_count ) {
@@ -44,7 +44,7 @@ if ( post_password_required() ) {
 				);
 			}
 			?>
-		</h2><!-- .comments-title -->
+		</h3><!-- .comments-title -->
 
 		<?php the_comments_navigation(); ?>
 
