907d5571229b0f5018a60fd4293fdc124b6e28b0
thingsym <thingsym@gmail.com>
2020-07-27 18:09:28 +0900

    fix: change Theme URI

--- templates/parts/theme-info.php
+++ templates/parts/theme-info.php
@@ -11,5 +11,5 @@
 
 ?>
 <div class="theme-info">
-<?php echo esc_html__( 'Proudly powered by ', 'foresight' ); ?><a href="<?php echo esc_url( __( 'https://wordpress.org/', 'foresight' ) ); ?>">WordPress</a> | <a href="<?php echo esc_url( 'https://github.com/thingsym/foresight' ); ?>">Foresight</a> <?php echo esc_html__( 'theme designed by thingsym', 'foresight' ); ?>
+<?php echo esc_html__( 'Proudly powered by ', 'foresight' ); ?><a href="<?php echo esc_url( __( 'https://wordpress.org/', 'foresight' ) ); ?>">WordPress</a> | <a href="<?php echo esc_url( 'https://homepage.thingslabo.com/product/wp-theme-foresight/' ); ?>">Foresight</a> <?php echo esc_html__( 'theme designed by thingsym', 'foresight' ); ?>
 </div><!-- .theme-info -->
