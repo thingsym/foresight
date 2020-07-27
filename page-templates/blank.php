486c9de1da643f1fd89f76c9f8db9f7239ff48dd
thingsym <thingsym@gmail.com>
2020-07-27 18:27:50 +0900

    perf: add Blank Page Template instead of Featured image Header Page Template

---
+++ page-templates/blank.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template Name: Blank Page Template
+ * Template Post Type: page
+ *
+ * The page template
+ *
+ * @package Foresight
+ */
+
+?>
+<?php get_header(); ?>
+
+<div class="container">
+<?php do_action( 'foresight/theme_hook/site/header/before' ); ?>
+<header class="site-header">
+<div class="header-container">
+<?php do_action( 'foresight/theme_hook/site/header' ); ?>
+</div>
+</header>
+<?php do_action( 'foresight/theme_hook/site/header/after' ); ?>
+
+<?php do_action( 'foresight/theme_hook/site/content/before' ); ?>
+<div class="site-content">
+<main class="primary">
+<div class="main-container" id="main-container">
+<?php
+do_action( 'foresight/theme_hook/content/prepend' );
+while ( have_posts() ) :
+	the_post();
+	do_action( 'foresight/theme_hook/content/page/prepend' );
+	get_template_part( 'templates/content/blank', get_post_type() );
+	do_action( 'foresight/theme_hook/content/page/append' );
+endwhile;
+do_action( 'foresight/theme_hook/content/append' );
+?>
+</div>
+</main>
+</div>
+<?php do_action( 'foresight/theme_hook/site/content/after' ); ?>
+
+<?php get_footer(); ?>
