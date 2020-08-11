ce578aafdfa08799d2330789fa39f2e0ffd1217a
thingsym <thingsym@gmail.com>
2020-08-11 16:56:05 +0900

    fix: remove customize control 'excerpt_mblength'

--- src/js/admin/customize-control.js
+++ src/js/admin/customize-control.js
@@ -55,19 +55,6 @@
         setting.bind( visibility );
       });
 
-      wp.customize.control( 'foresight_excerpt_options[excerpt_mblength]', function( control ) {
-        var visibility = function() {
-          if ( 'summary' == setting.get() ) {
-            control.container.show( 0 );
-          } else {
-            control.container.hide( 0 );
-          }
-        };
-
-        visibility();
-        setting.bind( visibility );
-      });
-
     });
 
     wp.customize.control( 'blogdescription' ).priority( 20 );
