db976226792a748300fa048487828634d4663010
thingsym <thingsym@gmail.com>
2020-08-27 15:28:06 +0900

    chore: fix webpack.config.js, generate compressed and uncompressed files

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+var UnminifiedWebpackPlugin = require('unminified-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV,
@@ -9,16 +10,25 @@ module.exports = {
     'customize-preview': './src/js/admin/customize-preview.js',
   },
   output: {
-    filename: "[name].bundle.js",
+    filename: "[name].min.js",
     path: path.resolve(__dirname, 'js'),
   },
   module: {
     rules: [
       {
         test: /.jsx?$/,
-        loader: 'babel-loader',
         exclude: /node_modules/,
-      },
-    ],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime']
+          }
+        }
+      }
+    ]
   },
+  plugins: [
+    new UnminifiedWebpackPlugin()
+  ]
 };
