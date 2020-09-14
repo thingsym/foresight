cdd9f3af6ec9da3512fb1525fd8a9b8efcc0d966
thingsym <thingsym@gmail.com>
2020-09-14 16:09:04 +0900

    style: add space

--- tests/phpunit/test-setup-theme.php
+++ tests/phpunit/test-setup-theme.php
@@ -18,7 +18,7 @@ class Test_Setup_Theme extends WP_UnitTestCase {
 	 */
 	public function constructor() {
 		$this->assertEquals( 10, has_filter( 'after_setup_theme', [ $this->theme, 'setup' ] ) );
-		$this->assertEquals(10, has_filter( 'after_setup_theme', [ $this->theme, 'content_width' ] ) );
+		$this->assertEquals( 10, has_filter( 'after_setup_theme', [ $this->theme, 'content_width' ] ) );
 		$this->assertEquals( 1, has_filter( 'wp_head', [ $this->theme, 'print_meta' ] ) );
 	}
 
