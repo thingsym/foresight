dbaaf8d4fb884d33989fafcfed0256cf2969346c
thingsym <thingsym@gmail.com>
2021-11-09 17:20:05 +0900

    fix: remove hook for blank-page-without-header template

--- page-templates/blank-page-without-header.php
+++ page-templates/blank-page-without-header.php
@@ -12,7 +12,6 @@
 <?php get_header(); ?>
 
 <div class="container">
-<?php do_action( 'foresight/theme_hook/site/content/before' ); ?>
 <div class="site-content">
 <main class="primary">
 <div class="main-container" id="main-container">
@@ -22,10 +21,8 @@ if ( in_array( $post_type, [ 'post' ] ) ) {
 	$post_type = 'single';
 }
 
-do_action( 'foresight/theme_hook/content/prepend' );
 while ( have_posts() ) :
 	the_post();
-	do_action( 'foresight/theme_hook/content/' . $post_type . '/prepend' );
 	get_template_part( 'templates/content/blank', $post_type );
 	do_action( 'foresight/theme_hook/content/' . $post_type . '/append' );
 endwhile;
