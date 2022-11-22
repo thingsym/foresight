068f7413da5ef57edcd4f87af6989ff9dd7d95c0
thingsym <thingsym@gmail.com>
2022-11-22 18:35:33 +0900

    pref: change to button tag and add aria for drawer

--- templates/parts/global-navi.php
+++ templates/parts/global-navi.php
@@ -7,11 +7,11 @@
 
 ?>
 
-<div id="js-drawer-btn" class="drawer-btn" tabindex="0">
+<button id="js-drawer-btn" class="drawer-btn" tabindex="0" aria-controls="site-navi" aria-expanded="false">
 <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" style="pointer-events: none; display: block;">
 <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
 </svg>
-</div>
+</button>
 
 <div class="drawer-overlay"></div>
 
@@ -20,6 +20,7 @@ wp_nav_menu(
 	[
 		'theme_location'  => 'global',
 		'container'       => 'nav',
+		'container_id'    => 'site-navi',
 		'container_class' => 'site-navi drawer',
 		'menu_class'      => 'global-menu',
 		'fallback_cb'     => false,
