e0830b057aedfbbce4a8a061989d55f4175ca563
thingsym <thingsym@gmail.com>
2023-03-29 16:50:15 +0900

    perf: remove Wp_Custom_Css, Emoji, Image_Srcset

--- functions/loadup.php
+++ functions/loadup.php
@@ -29,7 +29,7 @@ $foresight_fn_entry_meta = new \Foresight\Functions\Custom_Entry_Meta\Custom_Ent
 $foresight_fn_copyright  = new \Foresight\Functions\Copyright\Copyright();
 
 new \Foresight\Functions\SEO\Meta_Description();
-new \Foresight\Functions\Wp_Custom_Css\Wp_Custom_Css();
+// new \Foresight\Functions\Wp_Custom_Css\Wp_Custom_Css();
 
-new \Foresight\Functions\Emoji\Emoji();
-new \Foresight\Functions\Image_Srcset\Image_Srcset();
+// new \Foresight\Functions\Emoji\Emoji();
+// new \Foresight\Functions\Image_Srcset\Image_Srcset();
